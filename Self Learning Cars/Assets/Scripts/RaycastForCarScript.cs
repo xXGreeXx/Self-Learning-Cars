fb3f5376ba8fdd42bda9a3cec944deeb4fa8ddc3
xXGreeXx <lrhude.elijah@gmail.com>
2018-01-10 23:21:38 -0500

    Added stuff

---
+++ Self Learning Cars/Assets/Scripts/RaycastForCarScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastForCarScript : MonoBehaviour {
+
+    //define global variables
+    public static float sensor1Distance = 0;
+    public static GameObject line1Sensor;
+
+    public static float sensor2Distance = 0;
+    public static GameObject line2Sensor;
+
+    public static float sensor3Distance = 0;
+    public static GameObject line3Sensor;
+
+    //init
+    void Start ()
+    {
+        line1Sensor = new GameObject("Sensor");
+        line1Sensor.transform.SetParent(this.gameObject.transform);
+        line1Sensor.AddComponent<LineRenderer>();
+
+        line2Sensor = new GameObject("Sensor");
+        line2Sensor.transform.SetParent(this.gameObject.transform);
+        line2Sensor.AddComponent<LineRenderer>();
+
+        line3Sensor = new GameObject("Sensor");
+        line3Sensor.transform.SetParent(this.gameObject.transform);
+        line3Sensor.AddComponent<LineRenderer>();
+    }
+	
+	//update
+	void Update ()
+    {
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y + 200, transform.position.z + 350);
+        CastRay(origin, transform.right, line1Sensor);
+        CastRay(origin, transform.right + new Vector3(2, 0, 0), line2Sensor);
+        CastRay(origin, transform.right + new Vector3(-2, 0, 0), line3Sensor);
+    }
+
+    //cast ray
+    private void CastRay(Vector3 origin, Vector3 direction, GameObject lineObject)
+    {
+        LineRenderer line = lineObject.GetComponent<LineRenderer>();
+
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit;
+        bool rayHit = Physics.Raycast(ray, out hit, 1000);
+
+        line.startWidth = 10;
+        line.endWidth = 10;
+
+        line.SetPosition(0, origin);
+        if (rayHit)
+        {
+            line.SetPosition(1, hit.point);
+        }
+        else
+        {
+            line.SetPosition(1, ray.GetPoint(1000));
+        }
+    }
+}
